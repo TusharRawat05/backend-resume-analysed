04d24d61b1c926f9263a0e6d8653a8e75b71e4fb
TusharRawat05 <tusharrawat9311@gmail.com>
2026-04-22 21:33:10 +0530

    updated controller and middlewares

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,102 +3,57 @@ const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const tokenBlacklistModel = require("../models/blacklist.model")
 
-/**
- * @name registerUserController
- * @description register a new user, expects username, email and password in the request body
- * @access Public
- */
 async function registerUserController(req, res) {
-
     const { username, email, password } = req.body
-
     if (!username || !email || !password) {
         return res.status(400).json({
             message: "Please provide username, email and password"
         })
     }
-
     const isUserAlreadyExists = await userModel.findOne({
-        $or: [ { username }, { email } ]
+        $or: [{ username }, { email }]
     })
-
     if (isUserAlreadyExists) {
         return res.status(400).json({
             message: "Account already exists with this email address or username"
         })
     }
-
     const hash = await bcrypt.hash(password, 10)
-
-    const user = await userModel.create({
-        username,
-        email,
-        password: hash
-    })
-
+    const user = await userModel.create({ username, email, password: hash })
     const token = jwt.sign(
         { id: user._id, username: user.username },
         process.env.JWT_SECRET,
         { expiresIn: "1d" }
     )
-
-    res.cookie("token", token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none"
-})
-
-
     res.status(201).json({
         message: "User registered successfully",
+        token,
         user: {
             id: user._id,
             username: user.username,
             email: user.email
         }
     })
-
 }
 
-
-/**
- * @name loginUserController
- * @description login a user, expects email and password in the request body
- * @access Public
- */
 async function loginUserController(req, res) {
-
     const { email, password } = req.body
-
     const user = await userModel.findOne({ email })
-
     if (!user) {
-        return res.status(400).json({
-            message: "Invalid email or password"
-        })
+        return res.status(400).json({ message: "Invalid email or password" })
     }
-
     const isPasswordValid = await bcrypt.compare(password, user.password)
-
     if (!isPasswordValid) {
-        return res.status(400).json({
-            message: "Invalid email or password"
-        })
+        return res.status(400).json({ message: "Invalid email or password" })
     }
-
     const token = jwt.sign(
         { id: user._id, username: user.username },
         process.env.JWT_SECRET,
         { expiresIn: "1d" }
     )
-
-    res.cookie("token", token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none"
-})
     res.status(200).json({
         message: "User loggedIn successfully.",
+        token,
         user: {
             id: user._id,
             username: user.username,
@@ -107,37 +62,17 @@ async function loginUserController(req, res) {
     })
 }
 
-
-/**
- * @name logoutUserController
- * @description clear token from user cookie and add the token in blacklist
- * @access public
- */
 async function logoutUserController(req, res) {
-    const token = req.cookies.token
-
+    const authHeader = req.headers.authorization
+    const token = authHeader && authHeader.split(' ')[1]
     if (token) {
         await tokenBlacklistModel.create({ token })
     }
-
-    res.clearCookie("token")
-
-    res.status(200).json({
-        message: "User logged out successfully"
-    })
+    res.status(200).json({ message: "User logged out successfully" })
 }
 
-/**
- * @name getMeController
- * @description get the current logged in user details.
- * @access private
- */
 async function getMeController(req, res) {
-
     const user = await userModel.findById(req.user.id)
-
-
-
     res.status(200).json({
         message: "User details fetched successfully",
         user: {
@@ -146,10 +81,7 @@ async function getMeController(req, res) {
             email: user.email
         }
     })
-
 }
-
-
 
 module.exports = {
     registerUserController,
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,11 +1,9 @@
 const jwt = require("jsonwebtoken")
 const tokenBlacklistModel = require("../models/blacklist.model")
 
-
-
 async function authUser(req, res, next) {
-
-    const token = req.cookies.token
+    const authHeader = req.headers.authorization
+    const token = authHeader && authHeader.split(' ')[1]
 
     if (!token) {
         return res.status(401).json({
@@ -13,10 +11,7 @@ async function authUser(req, res, next) {
         })
     }
 
-    const isTokenBlacklisted = await tokenBlacklistModel.findOne({
-        token
-    })
-
+    const isTokenBlacklisted = await tokenBlacklistModel.findOne({ token })
     if (isTokenBlacklisted) {
         return res.status(401).json({
             message: "token is invalid"
@@ -25,19 +20,13 @@ async function authUser(req, res, next) {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
         req.user = decoded
-
         next()
-
     } catch (err) {
-
         return res.status(401).json({
             message: "Invalid token."
         })
     }
-
 }
-
 
 module.exports = { authUser }
